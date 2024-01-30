470404bd5dcf171fc3eaefc148dc34ee42ffb46a
Stephen <armmighty@gmail.com>
2024-01-30 17:02:27 -0500

    Created C++ Classes

---
+++ Source/PROJECT_Capstone/MainCharacterMovement.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "MainCharacterMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_CAPSTONE_API UMainCharacterMovement : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
